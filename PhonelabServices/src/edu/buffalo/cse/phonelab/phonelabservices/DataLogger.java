b20fba52bdbda6b723c8ffcc052b61cf499f0af1
Steve Ko <stevko@buffalo.edu>
2011-12-13 23:50:49 -0500

    Added the basic structure

---
+++ PhonelabServices/src/edu/buffalo/cse/phonelab/phonelabservices/DataLogger.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.buffalo.cse.phonelab.phonelabservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author stevko
+ *
+ */
+public class DataLogger extends Service {
+
+	/**
+	 * 
+	 */
+	public DataLogger() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ PhonelabServices/src/edu/buffalo/cse/phonelab/phonelabservices/PhonelabController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.buffalo.cse.phonelab.phonelabservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author stevko
+ *
+ */
+public class PhonelabController extends Service {
+
+	/**
+	 * 
+	 */
+	public PhonelabController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
