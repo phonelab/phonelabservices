588199a816cc1c232bb11ee2b727359c82fd0a66
Rishi Baldawa <baldawa.rishi@gmail.com>
2012-01-31 15:14:22 -0800

    notification for app

--- PhonelabServices/src/edu/buffalo/cse/phonelab/utilities/Util.java
+++ PhonelabServices/src/edu/buffalo/cse/phonelab/utilities/Util.java
@@ -3,7 +3,14 @@
  */
 package edu.buffalo.cse.phonelab.utilities;
 
+import edu.buffalo.cse.phonelab.controller.PhoneLabMainView;
+import edu.buffalo.cse.phonelab.controller.R;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.telephony.TelephonyManager;
 
 public class Util {
@@ -11,5 +18,35 @@ public class Util {
 	public static String getDeviceId (Context context) {
 		TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 		return tm.getDeviceId();
+	}
+	
+	/**
+	 * send Notification message to device user and points him to PhonelabMainView Class
+	 * 
+	 * @param context
+	 * @param Header
+	 * @param Message
+	 */
+	private void NofityUser(Context context,String Header, String Message) {
+		String ns = Context.NOTIFICATION_SERVICE;
+		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+		int icon = R.drawable.ic_launcher;
+		CharSequence tickerText = Header;
+		long when = System.currentTimeMillis();
+
+		Notification notification = new Notification(icon, tickerText, when);
+
+		Context appContext = context.getApplicationContext();
+		CharSequence contentTitle = Header;
+		CharSequence contentText = Message;
+		
+		Intent notificationIntent = new Intent(context, PhoneLabMainView.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+				notificationIntent, 0);
+
+		notification.setLatestEventInfo(appContext, contentTitle, contentText,
+				contentIntent);
+
+		mNotificationManager.notify(1, notification);
 	}
 }
